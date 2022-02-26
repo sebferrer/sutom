7e5ed9ab64517935b2f2cb430142d0760cc3d0d7
sebferrer <sebastien@sebferrer.fr>
2022-02-26 23:15:46 +0100

    word-grid component

--- front/src/app/ui/word-grid/word-grid.component.ts
+++ front/src/app/ui/word-grid/word-grid.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+
+const DEFAULT_NB_ROWS = 6;
 
 @Component({
 	selector: 'app-word-grid',
@@ -8,9 +10,43 @@ import { Observable } from 'rxjs';
 })
 export class WordGridComponent implements OnInit {
 
+	@Input()
+	public word: string;
+
+	@Input()
+	public nbRows: number;
+
+	public nbLetters: number;
+	public firstLetter: string;
+
+	public grid: Array<Array<string>>;
+
 	constructor(
-	) { }
+	) {
+	}
 
 	public ngOnInit(): void {
+		this.nbRows = this.nbRows == null ? DEFAULT_NB_ROWS : this.nbRows;
+		this.nbLetters = this.word.length;
+		this.firstLetter = this.word[0];
+
+		this.initGrid();
+	}
+
+	public initGrid() {
+		this.grid = new Array<Array<string>>();
+		for (let i = 0; i < this.nbRows; i++) {
+			let row = new Array();
+			for (let j = 0; j < this.nbLetters; j++) {
+				if (i === 0 && j === 0) {
+					row.push(this.firstLetter);
+				} else if (i === 0) {
+					row.push(".");
+				} else {
+					row.push(" ");
+				}
+			}
+			this.grid.push(row);
+		}
 	}
 }
